6ac490f4d38ecc197dfe95044e6005e794f2ae7f
Olesia Stepanova <git.user.ikt63@yandex.ru>
2026-02-22 20:01:03 +0300

    Basic arithmetic operations

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text=new Scanner(System.in).nextLine();
-        System.out.println("Длина текста :"+text.length());
+        System.out.println("Введите первое число: ");
+        int firstNumber=new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int sum = firstNumber+secondNumber;
+        int difference=firstNumber-secondNumber;
+        int mult=firstNumber*secondNumber;
+          double division= (double) firstNumber/secondNumber;
+        System.out.println(firstNumber+"+"+secondNumber+"="+sum);
+        System.out.println(firstNumber+"-"+secondNumber+"="+difference);
+        System.out.println(firstNumber+"*"+secondNumber+"="+mult);
+        System.out.println(firstNumber+"/"+secondNumber+"="+division);
     }
 }
